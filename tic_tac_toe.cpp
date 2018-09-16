8ad7f8635e213925de711fd1be3a84250c3a4dc5
Shajal Kumer Sarker <shajalkumer@gmail.com>
2018-09-17 01:34:49 +0600

    File Uploaded

---
+++ tic_tac_toe.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+char matrix[3][3] = {
+    {'1', '2', '3'},
+    {'4','5','6'},
+    {'7','8','9'}
+    };
+char player = 'X';
+int n = 0;
+
+void draw() {
+    system("cls");
+    cout<<"Tic Tac Toe v1.0"<<endl;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cout<<" "<<matrix[i][j];
+        }
+        cout<<endl;
+    }
+}
+
+void input() {
+ int a;
+ cout << "It's " << player << " turn. " << "Enter the number of the field: ";
+ cin >> a;
+ if (a == 1) {
+    if (matrix[0][0] == '1') {
+        matrix[0][0] = player;
+    } else {
+        cout<<"Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 2) {
+    if (matrix[0][1] == '2') {
+        matrix[0][1] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 3) {
+    if (matrix[0][2] == '3') {
+        matrix[0][2] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 4) {
+    if (matrix[1][0] == '4') {
+        matrix[1][0] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 5) {
+    if (matrix[1][1] == '5') {
+        matrix[1][1] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 6) {
+    if (matrix[1][2] == '6') {
+        matrix[1][2] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 7) {
+    if (matrix[2][0] == '7') {
+        matrix[2][0] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 8) {
+    if (matrix[2][1] == '8') {
+        matrix[2][1] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ } else if (a == 9) {
+    if (matrix[2][2] == '9') {
+        matrix[2][2] = player;
+    } else {
+        cout << "Field is already in use try again!"<<endl;
+        input();
+    }
+ }
+}
+
+void togglePlayer() {
+    if(player == 'X')
+        player = '0';
+    else
+        player = 'X';
+}
+
+char win() {
+    /*
+    00 01 02
+    10 11 12
+    20 21 22
+    */
+     // For player X
+    // For row
+
+    if(matrix[0][0] =='X' && matrix[0][1] == 'X' && matrix[0][2] == 'X')
+        return 'X';
+    if(matrix[1][0] =='X' && matrix[1][1] =='X' && matrix[1][2] =='X')
+        return 'X';
+    if(matrix[2][0] =='X' && matrix[2][1] =='X' && matrix[2][2] =='X')
+        return 'X';
+    // For column
+    if(matrix[0][0] =='X'&& matrix[1][0] =='X'&& matrix[2][0] =='X')
+        return 'X';
+    if(matrix[0][1] =='X'&& matrix[1][1] =='X'&& matrix[2][1] =='X')
+        return 'X';
+    if(matrix[0][2] =='X'&& matrix[1][2] =='X'&& matrix[2][2] =='X')
+        return 'X';
+    // Diagonal
+     if(matrix[0][0] =='X' && matrix[1][1] =='X' && matrix[2][2] =='X')
+        return 'X';
+    if(matrix[0][2] =='X'&& matrix[1][1] =='X' && matrix[2][0] =='X')
+        return 'X';
+
+
+    // For player O
+    // For row
+    if (matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O')
+        return 'O';
+    if (matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O')
+        return 'O';
+    if (matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+    // For column
+    if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O')
+        return 'O';
+    if (matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O')
+        return 'O';
+    if (matrix[0][2] == 'O' && matrix[1][2] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+    // Diagonal
+    if (matrix[0][0] == 'O' && matrix[1][1] == 'O' && matrix[2][2] == 'O')
+        return 'O';
+    if (matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O')
+        return 'O';
+
+    return '/';
+
+}
+
+
+int main() {
+
+draw();
+while(1) {
+    n++;
+    input();
+    draw();
+    if(win() == 'X'){
+        cout<<"\nX win!"<<endl;
+        break;
+    }else if(win() == 'O') {
+        cout<<"\nO win!"<<endl;
+        break;
+    } else if(win() == '/' && n == 9) {
+        cout<<"Game draw!"<<endl;
+        break;
+    }
+    togglePlayer();
+
+}
+
+
+
+
+
+
+return 0;
+}
